18d5032725983db78885bfe4175bb7bf001629db
Dima Bielov <97897846+Mandalayyy@users.noreply.github.com>
2023-03-08 19:24:06 +0200

    add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,27 @@ export class AppComponent implements OnInit {
   title = 'my-app';
   allData: any
   curUSD:number|undefined = 0;
+  curEU: number | undefined = 0;
+  curGBP: number| undefined = 0;
   valueFrom: number=0;
   valueTo: number=0;
   flagFrom: string = "USD";
   flagTo: string = "USD";
+  currencys = [
+    {name: 'USD', img: '../assets/usa.png'},
+    {name: 'EUR',img: '../assets/eu.png'},
+    {name: 'UAH',img: '../assets/ua.png'}
+  ]
+  selectedCurrencyFrom = {name: 'USD', img: '../assets/usa.png'};
+  selectedCurrencyTo = {name: 'USD', img: '../assets/usa.png'};
+
   
   constructor(private data: CurrencyConverterService) {}
 
   ngOnInit(): void{
     this.getCurrencyUSD("USD","UAH",1);
+    this.getCurrencyEU("EUR","UAH",1);
+    this.getCurrencyGBP("GBP","UAH",1);
   }
 
   getCurrencyUSD(have:string,want:string,amount:number){
@@ -27,6 +39,19 @@ export class AppComponent implements OnInit {
       this.curUSD = myData.new_amount
     })
   }
+
+  getCurrencyEU(have:string,want:string,amount:number){
+    this.data.getData(have,want,amount).subscribe((myData: response)=>{
+      this.curEU = myData.new_amount
+    })
+  }
+
+  getCurrencyGBP(have:string,want:string,amount:number){
+    this.data.getData(have,want,amount).subscribe((myData: response)=>{
+      this.curGBP = myData.new_amount
+    })
+  }
+
   getCurrencyValueTo(have:string,want:string,amount:number){
     this.data.getData(have,want,amount).subscribe((myData: response)=>{
       if(myData.new_amount)
@@ -66,14 +91,40 @@ export class AppComponent implements OnInit {
   }
 
   selectChangeTo(event:any){
-    this.flagTo = event.target.value;
+    let x =  this.currencys.find(e => e.name == event.value);
+    if(x){
+      this.selectedCurrencyTo = x;
+    }
+   console.log(this.selectedCurrencyTo) 
+   this.flagTo = event.value
     this.getCurrencyFlagTo(this.flagTo,this.flagFrom,this.valueTo)
   }
 
 
   selectChangeFrom(event:any){
-    this.flagFrom = event.target.value;
+    let x =  this.currencys.find(e => e.name == event.value);
+    if(x){
+      this.selectedCurrencyFrom = x;
+    }
+    this.flagFrom = event.value;
+    console.log(event)
     this.getCurrencyFlagFrom(this.flagFrom,this.flagTo,this.valueFrom)
+  }
+
+  onButtonClick(){
+    let flag;
+    let value;
+    flag = this.flagTo;
+    value = this.valueTo;
+    this.flagTo = this.flagFrom;
+    this.valueTo = this.valueFrom
+    this.flagFrom = flag;
+    this.valueFrom = value;
+    flag = this.selectedCurrencyTo;
+    this.selectedCurrencyTo = this.selectedCurrencyFrom;
+    this.selectedCurrencyFrom = flag
+    console.log('to', this.selectedCurrencyTo)
+    console.log('from', this.selectedCurrencyFrom)
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
